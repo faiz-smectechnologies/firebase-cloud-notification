4a494dfe8b284005b92be75088f18e9dde7355d3
faiz-smectechnologies <muhammedfaiz@smectechnologies.com>
2023-09-15 17:34:31 +0530

    firebase push message integrated

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/8.6.2/firebase-messaging.js");
+
+// Initialize Firebase with your Firebase configuration
+firebase.initializeApp({
+    apiKey: "AIzaSyAVBa0P9u20drZiCOu-hDFHh-RHk8yBZPk",
+    authDomain: "push-notification-ada5a.firebaseapp.com",
+    projectId: "push-notification-ada5a",
+    storageBucket: "push-notification-ada5a.appspot.com",
+    messagingSenderId: "350988701977",
+    appId: "1:350988701977:web:00e20515ea7f8f09cff588",
+    measurementId: "G-4T2HGJC39T"
+});
+
+// Retrieve an instance of Firebase Messaging
+const messaging = firebase.messaging();
+
+
+
+
